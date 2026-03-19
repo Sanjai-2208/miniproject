54eed65bfa9b41a862fc41ba2019cc1b5be5c03b
Sanjai-2208 <saisrisanjai2004@gmail.com>
2026-03-19 10:08:10 +0530

    codes for project

---
+++ demo.js
@@ -0,0 +1,49 @@
+// console.log("hello .................welcome")
+// var number = 10
+// number=25
+// console.log(number)
+// var number = "hello"
+// console.log(number)
+// if(true)
+// {
+//     var number = 10
+    
+// console.log(number)
+// var number1=25
+// }
+// console.log(number1)
+
+
+// MATH Function
+
+
+// PI VALUES
+let c= Math.PI;
+console.log(c);
+
+// Math Round
+c = Math.round(5.2);
+console.log(c);
+
+// Math Floor
+
+c= Math.floor(30.00989);
+console.log (c);
+
+// Math Cos
+c= Math.cos(45);
+console.log(c);
+
+// Math Tan
+c= Math.tan(30);
+console.log(c);
+
+// Math Sqrt
+
+c=Math.sqrt(40);
+console.log(c);
+
+// Math Pow
+c=Math.pow(40,3);
+console.log(c);
+
